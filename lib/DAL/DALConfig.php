11c56cc343f6d5de3040bc68f387efe6d7b8795d
ning <buptning@gmail.com>
2013-06-03 10:22:27 +0800

    Update DALConfig.php
    去掉主机和密码信息

--- lib/DAL/DALConfig.php
+++ lib/DAL/DALConfig.php
@@ -20,16 +20,16 @@ unset($last);
 class DALConfig
 {
     public static $config_mysql=array(
-        "db0"=>array("uname"=>"sogoulog","pwd"=>"sogoulog","host"=>"10.16.135.129","db_name"=>"logadmin","encode"=>"UTF8"),
-        "db1"=>array("uname"=>"iestat","pwd"=>"iestat","host"=>"10.12.9.160","db_name"=>"iestat","encode"=>"gbk"),
-        "db2"=>array("uname"=>"sogouie","pwd"=>"sogouie","host"=>"10.12.9.160","db_name"=>"sogouie","encode"=>"gbk"),
+        "db0"=>array("uname"=>"","pwd"=>"","host"=>"","db_name"=>"","encode"=>"UTF8"),
+        "db1"=>array("uname"=>"","pwd"=>"","host"=>"","db_name"=>"","encode"=>"gbk"),
+        "db2"=>array("uname"=>"","pwd"=>"","host"=>"","db_name"=>"","encode"=>"gbk"),
     );
     //memcache的信息
-    public static $config_memcache=array("host"=>"10.17","port"=>11211) ;
+    public static $config_memcache=array("host"=>"","port"=>11211) ;
     //redis配置信息
     //redis配置信息
     public static $config_redis=array(
-        0=>array("host"=>"Redis01.sogou-op.org","port"=>6379),
-        1=>array("host"=>"10.11.133","port"=>6379)
+        0=>array("host"=>"","port"=>6379),
+        1=>array("host"=>"","port"=>6379)
     );
 }
